b800ad5f1756a5b5a96447be66ae4c3cf1df396a
Mardax <62964422+mardax007@users.noreply.github.com>
2023-01-30 15:25:01 +0100

    feat: :sparkles: Better intro

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -5,9 +5,11 @@
     import Exp from "$lib/components/Exp.svelte";
 
     let intro: DocumentData = {startDate: 1472319999999};
+    let introText = "";
 
     async function loadIntroduction() {
         intro = await getIntroduction();
+        introText = (intro.text.replace("[INSERTNAME]", `<span>${intro.name}</span>`).replace("[INSERTSCHOOL]", `<a href="${intro.schoolInfo.url}"><span style="color: ${intro.schoolInfo.color};">${intro.schoolInfo.name}</span></a>`).replace("[INSERTSCHOOLYEAR]", `<span class='excludeHover'>${intro.schoolInfo.yearName}</span>`).replace("[INSERTLOCATION]", `<span class='excludeHover' style="color: ${intro.schoolInfo.locationColor};">${intro.schoolInfo.location}</span>`).replace("[INSERTSTUDIE]", `<span class='excludeHover'>${intro.schoolInfo.studie}</span>`)).split("[INSERTTIME]")
         return intro;
     }
 </script>
@@ -21,9 +23,9 @@
             <h1>{@html intro.title}</h1>
         </div>
         <p id="introText">
-            {@html (intro.text.replace("[INSERTNAME]", `<span>${intro.name}</span>`).replace("[INSERTSCHOOL]", `<a href="${intro.schoolInfo.url}"><span style="color: ${intro.schoolInfo.color};">${intro.schoolInfo.name}</span></a>`).replace("[INSERTSCHOOLYEAR]", `<span class='excludeHover'>${intro.schoolInfo.yearName}</span>`).replace("[INSERTLOCATION]", `<span class='excludeHover' style="color: ${intro.schoolInfo.locationColor};">${intro.schoolInfo.location}</span>`).replace("[INSERTSTUDIE]", `<span class='excludeHover'>${intro.schoolInfo.studie}</span>`)).split("[INSERTTIME]")[0]}
+            {@html introText[0]}
             <Exp startDate={intro.startDate} />
-            {@html (intro.text.replace("[INSERTNAME]", `<span>${intro.name}</span>`).replace("[INSERTSCHOOL]", `<a href="${intro.schoolInfo.url}"><span style="color: ${intro.schoolInfo.color};">${intro.schoolInfo.name}</span></a>`).replace("[INSERTSCHOOLYEAR]", `<span class='excludeHover'>${intro.schoolInfo.yearName}</span>`).replace("[INSERTLOCATION]", `<span class='excludeHover' style="color: ${intro.schoolInfo.locationColor};">${intro.schoolInfo.location}</span>`).replace("[INSERTSTUDIE]", `<span class='excludeHover'>${intro.schoolInfo.studie}</span>`)).split("[INSERTTIME]")[1]}
+            {@html introText[1]}
         </p>
     </div>
 {/await}
