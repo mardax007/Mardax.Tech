c748d42cafd93a8b69c196687dbb88c793546ab4
mardax007 <62964422+mardax007@users.noreply.github.com>
2023-07-10 14:46:47 +0200

    feat: :sparkles: added footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        const socials = document.getElementsByClassName("social")
+
+        console.log(socials)
+
+        for (let i = 0; i < socials.length; i++) {
+            const social = socials[i];
+
+            social?.addEventListener('mousemove', (e) => {
+                const distanceX = (e.clientX - social.getBoundingClientRect().left - social.clientWidth / 2);
+                const distanceY = (e.clientY - social.getBoundingClientRect().top - social.clientHeight / 2);
+
+
+                social.animate(
+                    [
+                        { transform: `translate(${distanceX / 20}px, ${distanceY / 10}px)` },
+                    ],
+                    {
+                        duration: 500,
+                        easing: 'ease-in-out',
+                        fill: 'forwards',
+                    }
+                );
+            });
+
+            social?.addEventListener('mouseleave', () => {
+                social.animate(
+                    [
+                        { transform: `translate(0)` },
+                    ],
+                    {
+                        duration: 500,
+                        easing: 'ease-in-out',
+                        fill: 'forwards',
+                    }
+                );
+            });
+        };
+    });
+</script>
+
+<div id="footer">
+    <div id="socials">
+        <a class="social" href="https://github.com/mardax007">
+            <img alt="Github logo" src="./github.svg" />
+        </a>
+        <a class="social" href="https://www.linkedin.com/in/vriesman/">
+            <img alt="LinkedIn logo" src="./linkedIn.svg" />
+        </a>
+        <a class="social" href="mailto:mardax007@gmail.com">
+            <img src="./mail.svg" alt="email" />
+        </a>
+    </div>
+    <p>Copyright © 2023 Martijn Vriesman. All rights reserved.</p>
+</div>
+
+<style lang="scss">
+	@import '../../app.scss';
+
+    #footer {
+        width: 100%;
+        margin-top: 4rem;
+        height: 12rem;
+        background-color: #f1f1f1;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        p {
+            text-align: center;
+        }
+
+        #socials {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+
+            a {
+                margin: 0 1rem;
+                padding: .5rem;
+                border-radius: calc($borderRadius / 2);
+
+                &:hover {
+                    transform: scale(1.1);
+                    background-color: #ffffff;
+                }
+            }
+
+            img {
+                // center the image
+                display: flex;
+                cursor: pointer;
+                width: 3rem;
+                height: 3rem;
+
+                transition: all 0.1s ease-in-out;
+            }
+        }
+    }
+</style>
