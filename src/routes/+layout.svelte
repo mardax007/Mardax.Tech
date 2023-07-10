3296f180acaf3c26fc830e9833a1bef391aa9f58
mardax007 <62964422+mardax007@users.noreply.github.com>
2023-07-10 14:47:52 +0200

    feat: :chart_with_upwards_trend: moved analytics to the layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { getFirebaseApp } from "$lib/scripts/information";
+	import { onMount } from "svelte";
+
+    onMount(async () => {
+        const getAnalytics = (await import('firebase/analytics')).getAnalytics;
+        getAnalytics(getFirebaseApp());
+    });
+</script>
+
+<slot></slot>
+
+<style lang="scss">
+</style>
